f4c0c753d8ebda25971ec425720f07749e1117e6
c-breder <c.brederfigueiredo@gmail.com>
2025-08-27 09:57:02 -0300

    27-08

--- platformio/src/main.ino.cpp
+++ platformio/src/main.ino.cpp
@@ -1,4 +1,7 @@
-//LIBRARIES
+# 1 "C:\\Users\\cbred\\AppData\\Local\\Temp\\tmp03l5nggr"
+#include <Arduino.h>
+# 1 "C:/Users/cbred/Projetos/contato-hardware/platformio/src/main.ino"
+
 #include "MPU6050_6Axis_MotionApps20.h"
 #include <esp_now.h>
 #include <WiFi.h>
@@ -6,39 +9,45 @@
     #include "Wire.h"
 #endif
 
-//MPU Initialization
+
 MPU6050 mpu;
 #define INTERRUPT_PIN 35
-bool dmpReady = false;  
-uint8_t mpuIntStatus;   
-uint8_t devStatus;      
-uint16_t packetSize;   
-uint16_t fifoCount;    
+bool dmpReady = false;
+uint8_t mpuIntStatus;
+uint8_t devStatus;
+uint16_t packetSize;
+uint16_t fifoCount;
 uint8_t fifoBuffer[64];
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+Quaternion q;
+VectorInt16 aa;
+VectorInt16 aaReal;
+VectorInt16 aaWorld;
+VectorFloat gravity;
+float euler[3];
+float ypr[3];
 
-volatile bool mpuInterrupt = false;    
+volatile bool mpuInterrupt = false;
+void dmpDataReady();
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
+void setup();
+void loop();
+int touchRead();
+#line 27 "C:/Users/cbred/Projetos/contato-hardware/platformio/src/main.ino"
 void dmpDataReady() {
     mpuInterrupt = true;
 }
 
-//ESPNOW Initialization
-uint8_t broadcastAddress[] = {0x84, 0xcc, 0xa8, 0x5d, 0x63, 0x90};
-// Base 3: 0xcc, 0xdb, 0xa7, 0x91, 0x47, 0xe8
-// Base 4: 0xb0, 0xa7, 0x32, 0xd7, 0x58, 0x7c
-// Base 5: 0x40, 0x22, 0xd8, 0x4f, 0x5f, 0xd8
-// Base 6: 0x84, 0xcc, 0xa8, 0x5d, 0x63, 0x90
-// Base 7: 0xcc, 0xdb, 0xa7, 0x91, 0x53, 0x00 
 
-//Message Struct
+uint8_t broadcastAddress[] = {0x84, 0xcc, 0xa8, 0x5d, 0x63, 0x90};
+
+
+
+
+
+
+
 typedef struct struct_message {
-    int id; // must be unique for each sender board
+    int id;
     int gyro;
     int accel;
     int touch;
@@ -47,88 +56,88 @@ typedef struct struct_message {
 struct_message MIDImessage;
 esp_now_peer_info_t peerInfo;
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  //Serial.print("\r\nLast Packet Send Status:\t");
-  //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+
+
 }
 
-//Variables
+
 float ypr_mod = 0;
 int mediaAccel;
-int buttonState;             // the current reading from the input pin
+int buttonState;
 int lastButtonState = 0;
 
 
 void setup() {
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
-        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+        Wire.setClock(400000);
     #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
         Fastwire::setup(400, true);
     #endif
   Serial.begin(115200);
-  //GYRO Initialization
+
   Serial.println(F("Initializing I2C devices..."));
   mpu.initialize();
   pinMode(INTERRUPT_PIN, INPUT);
   Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
   Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-  //while (Serial.available() && Serial.read()); // empty buffer
-  //while (!Serial.available());                 // wait for data
-  //while (Serial.available() && Serial.read()); // empty buffer again 
+
+
+
   Serial.println(F("Initializing DMP..."));
   devStatus = mpu.dmpInitialize();
 
-  //Offsets
-  mpu.setXGyroOffset(220);//220
-  mpu.setYGyroOffset(76);//76
-  mpu.setZGyroOffset(-85);//-85
-  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788);
   if (devStatus == 0) {
-          // Calibration Time: generate offsets and calibrate our MPU6050
+
           mpu.CalibrateAccel(6);
           mpu.CalibrateGyro(6);
           mpu.PrintActiveOffsets();
-          // turn on the DMP, now that it's ready
+
           Serial.println(F("Enabling DMP..."));
           mpu.setDMPEnabled(true);
-  
-          // enable Arduino interrupt detection
+
+
           Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
           Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
           Serial.println(F(")..."));
           attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
           mpuIntStatus = mpu.getIntStatus();
-  
-          // set our DMP Ready flag so the main loop() function knows it's okay to use it
+
+
           Serial.println(F("DMP ready! Waiting for first interrupt..."));
           dmpReady = true;
-  
-          // get expected DMP packet size for later comparison
+
+
           packetSize = mpu.dmpGetFIFOPacketSize();
-      } 
+      }
       else {
-          // ERROR!
-          // 1 = initial memory load failed
-          // 2 = DMP configuration updates failed
-          // (if it's going to break, usually the code will be 1)
+
+
+
+
           Serial.print(F("DMP Initialization failed (code "));
           Serial.print(devStatus);
           Serial.println(F(")"));
       }
 
 
-  //ESPNOW Initialization
+
   WiFi.mode(WIFI_STA);
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
   esp_now_register_send_cb(OnDataSent);
-  // Register peer
+
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
+  peerInfo.channel = 0;
   peerInfo.encrypt = false;
-  // Add peer        
+
   if (esp_now_add_peer(&peerInfo) != ESP_OK){
     Serial.println("Failed to add peer");
     return;
@@ -137,13 +146,13 @@ void setup() {
 
 
 void loop() {
-  
-    MIDImessage.id = 6; 
-    // if programming failed, don't try to do anything
+
+    MIDImessage.id = 6;
+
     if (!dmpReady) return;
-    // read a packet from FIFO
-    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { 
-      
+
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {
+
         mpu.dmpGetQuaternion(&q, fifoBuffer);
         mpu.dmpGetGravity(&gravity, &q);
         mpu.dmpGetAccel(&aa, fifoBuffer);
@@ -152,15 +161,15 @@ void loop() {
         mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
         ypr_mod = ypr[2] * 180/M_PI;
 
-        int pressed = touchRead();  
-    
+        int pressed = touchRead();
+
         MIDImessage.gyro = ypr_mod;
         MIDImessage.accel = mediaAccel;
         MIDImessage.touch = pressed;
         esp_now_send(broadcastAddress, (uint8_t *) &MIDImessage, sizeof(MIDImessage));
         Serial.println("MIDI SENT");
    }
-   
+
   delay(10);
 }
 
@@ -173,9 +182,9 @@ int touchRead()
     {
       media += touchRead(T3);
       mediaAccel += aaReal.x;
-      
+
     }
-    media =  media/100;
+    media = media/100;
     mediaAccel = mediaAccel/100;
     if (media < 20)
     {

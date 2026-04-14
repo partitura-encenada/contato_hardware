f6f67b81e6320ce883f991306c8de8e98cb742bc
c-breder <c.brederfigueiredo@gmail.com>
2026-04-14 19:38:20 -0300

    equip_8 troca de base

--- platformio/src/main.ino
+++ platformio/src/main.ino
@@ -1,48 +1,21 @@
+#include <Arduino.h>
 #include <WiFi.h>
-#include <Wire.h>
-#include "SPIFFS.h"
-
-unsigned long startTime;
-const unsigned long testDuration = 180000; // 3 minutos (em ms)
-
-File logFile;
 
 void setup() {
   Serial.begin(115200);
+  delay(1000);
 
-  if (!SPIFFS.begin(true)) {
-    Serial.println("Erro ao montar SPIFFS");
-    return;
-  }
+  // MAC eFuse
+  uint64_t chipid = ESP.getEfuseMac();
+  Serial.printf("MAC eFuse: %04X%08X\n", (uint16_t)(chipid >> 32), (uint32_t)chipid);
 
-  // Cria/abre arquivo de log
-  logFile = SPIFFS.open("/log.txt", FILE_WRITE);
-  if (!logFile) {
-    Serial.println("Erro ao abrir arquivo");
-    return;
-  }
-
-  Serial.println("START");
-  logFile.println("START");
-
-  startTime = millis();
+  // MAC WiFi STA
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+  Serial.printf("MAC STA: 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X\n",
+    mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]
+  );
 }
 
 void loop() {
-  unsigned long currentTime = millis();
-
-  if (currentTime - startTime <= testDuration) {
-    String msg = "OK - tempo(ms): " + String(currentTime);
-
-    Serial.println(msg);
-    logFile.println(msg);
-
-    delay(1000);
-  } else {
-    Serial.println("Teste finalizado!");
-    logFile.println("Teste finalizado!");
-
-    logFile.close(); // importante fechar o arquivo
-    while (true); // para execução
-  }
 }
